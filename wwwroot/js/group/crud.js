9c9f7657a4dfa34ceba8464d1bd6b06583132a71
ferretisDev <ferretis.dev@outlook.com>
2023-05-31 16:39:36 -0600

    Update Group

--- wwwroot/js/group/crud.js
+++ wwwroot/js/group/crud.js
@@ -18,6 +18,27 @@
                 alert(errorThrow);
             })
     },
+    getSchoolsForUpdate: () => {
+        $.ajax({
+            method: "GET",
+            url: "../School/GetAll/",
+            type: "JSON",
+            data: {}
+        })
+            .done((data) => {
+                if (data.success == 1) {
+                    //$("#editSchoolFk").append(`<option value="">--Seleccione--</option>`);
+                    for (var i = 0; i < data.detail.entities.length; i++) {
+                        $("#editSchoolFk").append(`<option value="${data.detail.entities[i].id}">${data.detail.entities[i].name}</option>`);
+                    }
+                } else {
+
+                }
+            })
+            .fail((jqHR, textStatus, errorThrow) => {
+                alert(errorThrow);
+            })
+    },
     getGroups: () => {
         $.ajax({
             method: "GET",
@@ -85,7 +106,60 @@
                 alert(errorThrow);
             })
     },
-    editModal: () => {
+    editModal: (id) => {
+        $.ajax({
+            method: "GET",
+            url: `../Group/GetById?id=${id}`,
+            type: "JSON",
+            data: {}
+        })
+            .done((data) => {
+                if (data.success == 1) {
+                    $("#editModal").modal("show");
+                    $("#editId").val(data.detail.entity.id);
+                    $("#editName").val(data.detail.entity.name);
+                    $("#editMaxStudents").val(data.detail.entity.maxStudents);
+                    $("#editSchoolFk").val(data.detail.entity.schoolFk);
+                } else {
+                    alert(data.detail.error);
+                }
+            })
+            .fail((jqHR, textStatus, errorThrow) => {
+                alert(errorThrow);
+            })
+    },
+    closeEditModal: () => {
+        $("#editModal").modal("hide");
+    },
+    update: () => {
+        var id = $("#editId").val();
+        var name = $("#editName").val();
+        var maxStudents = $("#editMaxStudents").val();
+        var schoolFk = $("#editSchoolFk").val();
 
+        $.ajax({
+            method: "PATCH",
+            url: "../Group/Update/",
+            type: "JSON",
+            data: {
+                id: id,
+                name: name,
+                maxStudents: maxStudents,
+                schoolFk: schoolFk
+            }
+        })
+            .done((data) => {
+                if (data.success == 1) {
+                    alert(data.detail.message);
+                    $("#editModal").modal("hide");
+                    group.getGroups();
+                }
+                else {
+                    alert(data.detail.error);
+                }
+            })
+            .fail((jqHR, textStatus, errorThrow) => {
+                alert(errorThrow);
+            })
     }
 }
