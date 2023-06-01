fb23e26006ca63d00bc4ca735606dc226e03357f
ferretisDev <ferretis.dev@outlook.com>
2023-05-31 20:42:13 -0600

    Student List

---
+++ DetailViewModels/StudentDetailViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace ERPSchool.DetailViewModels
+{
+    public class StudentDetailViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public DateTime BirthDate { get; set; }
+
+        public int SexFk { get; set; }
+
+        public string SexName { get; set; } = null!;
+
+        public int GroupFk { get; set; }
+
+        public string GroupName { get; set; } = null!;
+    }
+}
---
+++ wwwroot/js/student/crud.js
@@ -0,0 +1,44 @@
+﻿var student = {
+    getStudents: () => {
+        $.ajax({
+            method: "GET",
+            url: "../Student/GetAllDetails/",
+            type: "JSON",
+            data: {}
+        })
+            .done((data) => {
+                if (data.success == 1) {
+                    var rows = "";
+
+                    for (i = 0; i < data.detail.entities.length; i++) {
+                        rows = rows.concat(`
+                        <tr>
+                            <td>
+                                ${data.detail.entities[i].name}
+                            </td>
+                            <td>
+                                ${data.detail.entities[i].birthDate.slice(0, 10)}
+                            </td>
+                            <td>
+                                ${data.detail.entities[i].sexName}
+                            </td> 
+                            <td>
+                                ${data.detail.entities[i].groupName}
+                            </td>  
+                            <td>
+					                <a href="javascript:void(0)" onclick="student.editModal(${data.detail.entities[i].id});"><i class="fa-solid icon fa-pen-to-square"></i></a> |
+                                    <a href="javascript:void(0)" onclick="student.deleteModal(${data.detail.entities[i].id});"><i class="fa-regular icon fa-trash-can"></i></a>
+				            </td>
+                        </tr>
+                        `)
+                        $("#students").html(rows);
+                    }
+                } else {
+                    alert(data.detail.error);
+                }
+            })
+            .fail((jqHR, textStatus, errorThrow) => {
+                alert(errorThrow);
+            })
+    }
+}
